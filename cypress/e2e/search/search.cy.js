9325ee31843496091f55e1c32f01515bba9bff79
ClecioSousa00 <cleciosouza015@gmail.com>
2023-09-24 21:03:56 -0300

    feat: created test search character

---
+++ cypress/e2e/search/search.cy.js
@@ -0,0 +1,25 @@
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+
+
+Given(/^I am on the character search page$/, () => {
+	cy.viewport(1280, 720)
+	cy.visit('/')
+});
+
+When(/^I type "([^"]*)" in the search box$/, (args1) => {
+	console.log(args1);
+	cy.get('[data-test="input-search"]').type(args1)
+});
+
+When(/^I press Enter$/, () => {
+	cy.get('[data-test="input-search"]').type('{enter}')
+});
+
+Then(/^I should see a list of "([^"]*)" characters$/, (args1) => {
+	cy.get('.mt-60 >').first().should('contain', args1)
+});
+
+
+Then(/^I should see the text "([^"]*)" indicating no results were found$/, (args1) => {
+	cy.get('.my-5').should('contain', 'Burrp!')
+});
